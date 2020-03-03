5e8b4ed9487f560e98caf45ea89b78a07ddb7b67
Pooja Maheshwar Guggari <pooja.maheshwar.guggari@gmail.com>
2020-03-03 13:14:01 -0500

    Clean up code

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,6 @@ import React from "react";
  * Functional component that renders Header
  */
 export default (props) => (
-    // <ul>
-    //     <li><a href="https://www.carbonite.com/" target="_blank" rel="noopener noreferrer">
-    //             <img src="/images/carbonite.png" alt="carbonite"></img>
-    //         </a>
-    //     </li>
-    //     <li style={{ "float": "right" }}>
-    //         <button onClick={ props.getUsers }>{ props.buttonText }</button>
-    //     </li>
-    // </ul>
     <div id="header">
         <h3>SINGLE PAGE REACT APPLICATION</h3>
         <div id="button-container">
